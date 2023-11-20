c6f5368d99fc3adbb41bc2be595dc07fbc4f8dce
Pistipisti94 <112941646+Pistipisti94@users.noreply.github.com>
2023-11-20 13:43:24 +0100

    adatok betöltése

--- 11 20 AJAX/mappa/AJAX.js
+++ 11 20 AJAX/mappa/AJAX.js
@@ -3,7 +3,7 @@ const inputusername = document.querySelector("#username");
 const inputdarab = document.querySelector("#darab");
 const buttonRead = document.querySelector("#read");
 const body = document.getElementsByTagName("body")[0];
-
+const cards = document.getElementById("cards");
 body.addEventListener("load", getAllUsers, false);//a lap betöltésekor jelennek meg az adatok
 buttonRead.addEventListener("click", getAllUsers);// a gombra kattintva jelennek meg az adatok
 
@@ -19,10 +19,15 @@ async function getAllUsers() {
 }
 function showAllUsers(params) {
     params.forEach(user => {
-        card = "<div class=\"card\" style=\"width: 18rem;\">"
-        +   "<img src=\"noImages.png\" class=\"card-img-top\" alt=\"noImages.png\">"   
-        +   "<div class=\"card-body\">"       
-        +   "<h5 class=\"card-title\">Card title</h5>",user.id;
+        card = document.createElement("div");
+        card.style.cssText = "width: 18rem;"
+        card.className = "card";
+        card.innerHTML = "<img src=\"noImages.png\" class=\"card-img-top\" alt=\"noImages.png\">"
+            + "<div class=\"card-body\">"
+            + "<h5 class=\"card-title\">" + user.id + " " + user.username + "</h5>"
+            + "<p class=\"card-text\">Ennyi darab: " + user.darab + "</p>"
+            + "</div>";
+        cards.appendChild(card);
+        console.log(user);
     });
 }
-
